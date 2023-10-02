230c9caa799916e2e63d5f11dc3b60f0567b4e63
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-10-01 21:36:16 -0400

    Create SparxEA.mm

---
+++ Sparx/SparxEA.mm
@@ -0,0 +1,112 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Sparx EA" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1696209697986" LINK="https://sparxsystems.com/enterprise_architect_user_guide/16.1/welcome/index.html">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#fdf6e3" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_solarized_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1704360557" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#002b36" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1704360557" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#fdf6e3" BACKGROUND_COLOR="#d33682" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#d33682"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1083719472" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_1083719472"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#fdf6e3" BACKGROUND_COLOR="#586e75" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#fdf6e3" BACKGROUND_COLOR="#657b83" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#fdf6e3" BACKGROUND_COLOR="#839496" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#fdf6e3" BACKGROUND_COLOR="#93a1a1" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#eee8d5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fdf6e3" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#fdf6e3">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="01 Welcome" POSITION="bottom_or_right" ID="ID_731201090" CREATED="1696209530040" MODIFIED="1696209533099"/>
+<node TEXT="02 Getting Started" POSITION="bottom_or_right" ID="ID_1705481608" CREATED="1696209533809" MODIFIED="1696209539481">
+<node TEXT="02.01 Why Enterprise Architect?" ID="ID_782880457" CREATED="1696209745689" MODIFIED="1696209760539">
+<node TEXT="Key Benefits" ID="ID_719566401" CREATED="1696209823195" MODIFIED="1696209827229"/>
+<node TEXT="Key Features" ID="ID_1542662236" CREATED="1696209827482" MODIFIED="1696209830740"/>
+<node TEXT="Key Usages" ID="ID_287752085" CREATED="1696209830906" MODIFIED="1696209832998"/>
+</node>
+<node TEXT="02.02 Editions &amp; Licensing" ID="ID_1482565837" CREATED="1696209761073" MODIFIED="1696209768350"/>
+<node TEXT="02.03 A Tutorial Modeling Session" ID="ID_1327237184" CREATED="1696209768641" MODIFIED="1696209780781"/>
+</node>
+<node TEXT="03 The Application Desktop" POSITION="bottom_or_right" ID="ID_832105389" CREATED="1696209539672" MODIFIED="1696209545931"/>
+<node TEXT="04 The Model Repository" POSITION="bottom_or_right" ID="ID_1684250790" CREATED="1696209576201" MODIFIED="1696209582178"/>
+<node TEXT="05 Modeling Fundamentals" POSITION="bottom_or_right" ID="ID_1795127784" CREATED="1696209582609" MODIFIED="1696209593299"/>
+<node TEXT="06 Modeling Language" POSITION="bottom_or_right" ID="ID_1584922604" CREATED="1696209593731" MODIFIED="1696209603210"/>
+<node TEXT="07 Modeling Domains" POSITION="bottom_or_right" ID="ID_1540565051" CREATED="1696209603377" MODIFIED="1696209609385"/>
+<node TEXT="08 Modeling Frameworks" POSITION="bottom_or_right" ID="ID_727304548" CREATED="1696209609538" MODIFIED="1696209616046"/>
+<node TEXT="09 Model Simulation" POSITION="bottom_or_right" ID="ID_401807959" CREATED="1696209616217" MODIFIED="1696209622516"/>
+<node TEXT="10 Model Publishing" POSITION="bottom_or_right" ID="ID_618668712" CREATED="1696209623400" MODIFIED="1696209628587"/>
+<node TEXT="11 Model Exchange" POSITION="bottom_or_right" ID="ID_921287924" CREATED="1696209628761" MODIFIED="1696209633299"/>
+<node TEXT="12 Model Security" POSITION="bottom_or_right" ID="ID_1496276866" CREATED="1696209633490" MODIFIED="1696209638300"/>
+<node TEXT="13 Teams &amp; Collaboration" POSITION="bottom_or_right" ID="ID_962836095" CREATED="1696209638481" MODIFIED="1696209644587"/>
+<node TEXT="14 Project Build &amp; Deploy" POSITION="bottom_or_right" ID="ID_1605011599" CREATED="1696209644737" MODIFIED="1696209651165"/>
+<node TEXT="15 Execution Analysis" POSITION="bottom_or_right" ID="ID_1653963009" CREATED="1696209651313" MODIFIED="1696209657650"/>
+<node TEXT="16 Add-ins &amp; Scripting" POSITION="bottom_or_right" ID="ID_1164084662" CREATED="1696209658009" MODIFIED="1696209664988"/>
+<node TEXT="17 Guide Books" POSITION="bottom_or_right" ID="ID_844509952" CREATED="1696209665195" MODIFIED="1696209671817"/>
+<node TEXT="18 Glossary" POSITION="bottom_or_right" ID="ID_1077034816" CREATED="1696209672010" MODIFIED="1696209675251"/>
+</node>
+</map>
