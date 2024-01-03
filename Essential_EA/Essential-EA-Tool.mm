a87acd81a13bf39475c62d9b1763cda22c9a2c7c
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-01-03 12:54:14 -0500

    update video 002

--- Essential_EA/Essential-EA-Tool.mm
+++ Essential_EA/Essential-EA-Tool.mm
@@ -85,6 +85,62 @@
 <node TEXT="Essential Project Home Wedsite" ID="ID_1034284306" CREATED="1704291571263" MODIFIED="1704291584772" LINK="https://enterprise-architecture.org/"/>
 <node TEXT="Essential University" ID="ID_473845825" CREATED="1704291589430" MODIFIED="1704291621066" LINK="https://enterprise-architecture.org/university/"/>
 </node>
-<node TEXT="002 Download Essential Open-Source Packages" POSITION="bottom_or_right" ID="ID_1416638504" CREATED="1704291625567" MODIFIED="1704291637760"/>
+<node TEXT="002 Download and Install Essential Open-Source Packages" POSITION="bottom_or_right" ID="ID_1416638504" CREATED="1704291625567" MODIFIED="1704304414899" LINK="https://youtu.be/JwERUE5VLFY">
+<icon BUILTIN="video"/>
+<node TEXT="Essential Open Source EA Tool download" ID="ID_1938095588" CREATED="1704299639920" MODIFIED="1704299663746" LINK="https://enterprise-architecture.org/products/essential-open-source/essential-os-download/"/>
+<node TEXT="Pre-Requisites" ID="ID_122086783" CREATED="1704303486541" MODIFIED="1704303783519">
+<node TEXT="1. Java: ensure version 1.8 or above&#xa;(I&apos;m using openjdk 11.0.16.1 which is ok)" ID="ID_1608757186" CREATED="1704303496696" MODIFIED="1704303552978"/>
+<node TEXT="2. Protege Ontology Editor" ID="ID_1981540024" CREATED="1704303557568" MODIFIED="1704303566615">
+<node TEXT="Only support v3.5, use the package in this site" ID="ID_677488506" CREATED="1704303566617" MODIFIED="1704303584688"/>
+<node TEXT="You can still use other Protege version along with this one for your other Ontologies, no conflict within Program Files" ID="ID_1785242354" CREATED="1704303585471" MODIFIED="1704303618647"/>
+<node TEXT="lax.nl.java.option.java.heap.size.max set to 2048000000" ID="ID_1828755099" CREATED="1704303625549" MODIFIED="1704303663256"/>
+</node>
+<node TEXT="3. Tomcat, only support v8 or v9" ID="ID_1916892068" CREATED="1704303665491" MODIFIED="1704303720596"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Essential Viewer can be used with any Java web application server but the installer assumes Tomcat
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="4. Optional components" ID="ID_792722817" CREATED="1704303730792" MODIFIED="1704303740000">
+<node TEXT="Database: you can ignore if you don&apos;t connect to DB" ID="ID_167877267" CREATED="1704303740002" MODIFIED="1704303759086"/>
+<node TEXT="JDBC Driver: for selected DB" ID="ID_1812656233" CREATED="1704303759792" MODIFIED="1704303769794"/>
+</node>
+</node>
+<node TEXT="Essential Core components" ID="ID_843052047" CREATED="1704303772206" MODIFIED="1704303796598">
+<node TEXT="1. Widgets (v6.7)" ID="ID_856759397" CREATED="1704303802661" MODIFIED="1704303823678">
+<node TEXT="A set of custom-built plug-ins for the Protege Ontology Editor. Run the installer and follow the on-screen instructions." ID="ID_1493198788" CREATED="1704303823680" MODIFIED="1704303833147"/>
+</node>
+<node TEXT="2. Meta-Model (v6.18)" ID="ID_570656221" CREATED="1704303834492" MODIFIED="1704303842625">
+<node TEXT="The main repository files for Essential. Save this to a user accessible area such as your Documents folder, unzip and then open using Protege." ID="ID_1350516432" CREATED="1704303842627" MODIFIED="1704303849811"/>
+<node TEXT="Rename the pprj file to your desired project name" ID="ID_689776567" CREATED="1704303853122" MODIFIED="1704303890938"/>
+</node>
+<node TEXT="3. Viewer (v6.18.7)" ID="ID_1162860886" CREATED="1704303897833" MODIFIED="1704303907579">
+<node TEXT="A Java Web Application for publishing, analysing and reporting against enterprise architecture models captured using the Essential Meta-Model. Copy this file to your Tomcat install &apos;webapps&apos; folder" ID="ID_1860810384" CREATED="1704303907581" MODIFIED="1704303915240"/>
+<node TEXT="Rename the war file and copy to your active Tomcat webapps folder, wait it extracted" ID="ID_505084072" CREATED="1704303915800" MODIFIED="1704303936666"/>
+</node>
+<node TEXT="4. Import Utility (v2.8)" ID="ID_1281495609" CREATED="1704303955223" MODIFIED="1704303966929">
+<node TEXT="A Java Web Application for importing existing content from spreadsheets into the Essential repository. Copy this file to your Tomcat install &apos;webapps&apos; folder" ID="ID_189712213" CREATED="1704303966931" MODIFIED="1704303976856"/>
+</node>
+<node TEXT="5. Import Utility Example Configuration (v2.0)" ID="ID_350975526" CREATED="1704303978006" MODIFIED="1704303988961">
+<node TEXT="An example configuration of Import Activities for the Import Utility. Save this to a user accessible area such as your Documents folder and select it from within the Import Utility&apos;s System Settings tab, using the &quot;Import Configuration...&quot; button" ID="ID_1670437532" CREATED="1704303988963" MODIFIED="1704303996114"/>
+</node>
+</node>
+<node TEXT="Installation Guide" ID="ID_1799156043" CREATED="1704304005527" MODIFIED="1704304012199">
+<node TEXT="Standalone Install" ID="ID_472717259" CREATED="1704304012201" MODIFIED="1704304032076" LINK="https://enterprise-architecture.org/docs/essential_os_installation/singleuser_install/"/>
+<node TEXT="Multi-User Install" ID="ID_1718746711" CREATED="1704304017966" MODIFIED="1704304037770" LINK="https://enterprise-architecture.org/docs/essential_os_installation/multiuser_install/"/>
+</node>
+<node TEXT="Practice after installation" ID="ID_827685949" CREATED="1704304269993" MODIFIED="1704304291373">
+<node TEXT="1. Open Meta-Model project in Protege" ID="ID_357787393" CREATED="1704304282078" MODIFIED="1704304303223"/>
+<node TEXT="2. Open Viewer through web browser using viewer URL" ID="ID_999901147" CREATED="1704304303840" MODIFIED="1704304335604"/>
+<node TEXT="3. Connect viewer from Protege and able to publish repository to viewer" ID="ID_1254315931" CREATED="1704304336288" MODIFIED="1704304357871"/>
+</node>
+</node>
 </node>
 </map>
