6f9fb7743cabec5b5f7df9c0e469c7541d5e6d5a
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-02-26 12:21:07 -0500

    update mindmap

--- BizArch/BAGuild_Learning.mm
+++ BizArch/BAGuild_Learning.mm
@@ -101,7 +101,7 @@
 </html>
 </richcontent>
 <node TEXT="1. Customer Service Definition &amp; Piroritization" ID="ID_866984744" CREATED="1708961457200" MODIFIED="1708961467319">
-<node TEXT="Defining Customer Services" ID="ID_1755048749" CREATED="1708961558852" MODIFIED="1708961568064">
+<node TEXT="Defining Customer Services" FOLDED="true" ID="ID_1755048749" CREATED="1708961558852" MODIFIED="1708961568064">
 <node TEXT="A direct one-on-one interaction between a consumer and company representative, commonly engaged when a consumer is buying a product or service" ID="ID_254337183" CREATED="1708961568067" MODIFIED="1708961611655"/>
 <node TEXT="Misconception #1: A customer service is only delivered by a company representative" ID="ID_59060021" CREATED="1708961616032" MODIFIED="1708961636270">
 <node TEXT="With hyper-automation and AI, customer services are increasingly being delivered by machines, not people" ID="ID_1103928210" CREATED="1708961636273" MODIFIED="1708961663215"/>
@@ -111,9 +111,232 @@
 <node TEXT="Customer services are more often than not experienced post-acquisition" ID="ID_376563717" CREATED="1708961822985" MODIFIED="1708961836615"/>
 <node TEXT="Consider use of a credit card, theater ticket, insurance policy, car rental, airline ticket, vehicle, software package shipment and other examples where customer services are primarily delivered post-purchase" ID="ID_247113003" CREATED="1708961836805" MODIFIED="1708961884937"/>
 </node>
+<node TEXT="A better definition of customer service: &quot;The support you offer your customers - both before and after they buy and use your products or services.&quot; (SalesForce.com" ID="ID_1010182801" CREATED="1708961933793" MODIFIED="1708961972912"/>
+</node>
+<node TEXT="Customer Service: A Key Element of Product" FOLDED="true" ID="ID_222006767" CREATED="1708962017602" MODIFIED="1708962030215">
+<node TEXT="Product is defined as:" ID="ID_1361970089" CREATED="1708962030217" MODIFIED="1708962039485">
+<node TEXT="&quot;The overall experience provided by the combination of goods and services to satisfy the customer&apos;s needs&quot;." ID="ID_839465845" CREATED="1708962039487" MODIFIED="1708962121370"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Source: Greg Geracie and Stephen Eppinger, <span style="font-style: italic;">The Guide to the Product Management and Marketing Body of Knowledge (ProdBOK)</span>&#xa0;&#xa0;(Association of International Product Marketing and Management, 2013)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_191026432" CREATED="1708962124654" MODIFIED="1708962159959"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Products by definition include goods <span style="text-decoration: underline;">and services</span>, because that is what the customer experiences before, during and after acquiring a product.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_151366532" CREATED="1708962160399" MODIFIED="1708962231357"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      In industry sections where service is an essential element of a product or the entirety of a product, customer service determines if an organization <span style="text-decoration: underline;">thrives, survives or ultimately fails</span>&#xa0;(蓬勃发展、生存或最终失败)
+    </p>
+  </body>
+</html>
+
+</richcontent>
 </node>
 </node>
-<node TEXT="2. Intersection of Customer Service Delivery &amp; Business Architecture" ID="ID_701704807" CREATED="1708961467518" MODIFIED="1708961478256"/>
+<node ID="ID_722416407" CREATED="1708962236646" MODIFIED="1708962291958"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      To be clear: Customer always refers to the end or external customer, client, consumer, constituent, patient or other beneficiary of an organization's goods and services - and <span style="font-weight: bold;">never</span>&#xa0;to an internal stakeholders
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="The state of customer service: Perception vs. Reality" FOLDED="true" ID="ID_1193197201" CREATED="1708962431136" MODIFIED="1708962448767">
+<node TEXT="Customer are not happy:" ID="ID_704138549" CREATED="1708962448769" MODIFIED="1708962901067"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Source: The State of Customer Service, Netomi, 2023, https://www.netomi.com/
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_917437114" CREATED="1708962457515" MODIFIED="1708962496262"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1 in 3 customers has <span style="text-decoration: underline;">screamed or sworn</span>&#xa0;&#xa0;(尖叫或发誓) at a customer service agent
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="73% of consumers reported having experienced a customer service agent being rude to them and 44% have had an agent become agitated (激动)." ID="ID_737621438" CREATED="1708962500031" MODIFIED="1708962552288"/>
+<node TEXT="3 in 5 people have hung up on an agent at least once out of frustration" ID="ID_1820402822" CREATED="1708962562903" MODIFIED="1708962588731"/>
+<node TEXT="39% of consumers have less patience now than they did prior to the pandemic" ID="ID_1519984841" CREATED="1708962589323" MODIFIED="1708962609122"/>
+<node TEXT="30% of customers will wait a maximum of 2 minutes for an agent on chat" ID="ID_837251251" CREATED="1708962609299" MODIFIED="1708962627861"/>
+<node TEXT="43% of consumers being long hold times are the most frustrating aspect of customer service" ID="ID_569955555" CREATED="1708962628098" MODIFIED="1708962650674"/>
+<node TEXT="25% have had to contact a company 3 or more times to resolve a single issue" ID="ID_256314875" CREATED="1708962651024" MODIFIED="1708962673620"/>
+<node TEXT="53% of consumers have publicly shamed a company for providing poor customer service" ID="ID_615738636" CREATED="1708962673817" MODIFIED="1708962697717"/>
+<node TEXT="62% of consumers have been ghosted by company&apos;s customer service call-back options" ID="ID_784022091" CREATED="1708962697904" MODIFIED="1708962720060"/>
+</node>
+<node TEXT="Business leaders seem unaware of the situation:" ID="ID_762180397" CREATED="1708962721401" MODIFIED="1708962735811">
+<node TEXT="There is 57% gap between how business leaders perceive that customers trust their organization vs. the reality of only 27% of customers actually trusting their organization" ID="ID_1782787219" CREATED="1708962735813" MODIFIED="1708962854198"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Source: PwC's 2023 Trust Survey, https://www.pwc.com/us/en/library/trust-in-business-survey-2023.html#key2
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Justifying across the Board Investment in Customer Services" ID="ID_1592282966" CREATED="1708963035269" MODIFIED="1708963048586">
+<node TEXT="Excellent customer service delivers financial benefits" FOLDED="true" ID="ID_1451741756" CREATED="1708963048588" MODIFIED="1708963343822" LINK="https://blog.hubspot.com/service/importance-customer-service">
+<node TEXT="Customer retention is cheaper than customer acquisition" ID="ID_5862625" CREATED="1708963068789" MODIFIED="1708963092303"/>
+<node TEXT="Customer will pay more to companies with better customer service" ID="ID_783337596" CREATED="1708963092663" MODIFIED="1708963103709"/>
+<node TEXT="Customer service grows customer lifetime value" ID="ID_345395319" CREATED="1708963103870" MODIFIED="1708963113228"/>
+<node TEXT="Customer service can lead to more revenue" ID="ID_1267912819" CREATED="1708963113397" MODIFIED="1708963122965"/>
+</node>
+<node TEXT="With the state of customer service poor and benefits of excellent service clear, one would assume that organizations would be laser focused on maximizing quality customer service" ID="ID_648754694" CREATED="1708963124022" MODIFIED="1708963168446"/>
+<node TEXT="Why then do organizations appear more focused on delivering internally targeted services vs. investing in delivering excellent service to the actual customer?" ID="ID_1213211999" CREATED="1708963373037" MODIFIED="1708963415856">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+<node TEXT="2. Intersection of Customer Service Delivery &amp; Business Architecture" ID="ID_701704807" CREATED="1708961467518" MODIFIED="1708961478256">
+<node TEXT="Business Architecture formalizes Customer Service Delivery" FOLDED="true" ID="ID_284361909" CREATED="1708964236125" MODIFIED="1708964250101">
+<node TEXT="Customer services are delivered through value streams, such as Execute Financial Transaction, Obtain Funding, Send Shipment, Service Vehicle, Settle Claim, Trade Financial Instrument and Take a Trip" ID="ID_1916338390" CREATED="1708964250103" MODIFIED="1708964325255"/>
+<node TEXT="Consier a Settle Claim Value Stream" ID="ID_851366126" CREATED="1708964330712" MODIFIED="1708964413000">
+<hook URI="settle-claim-value-stream.png" SIZE="0.5628518" NAME="ExternalObject"/>
+<node TEXT="Claims may be filed by the insured, a beneficiary of the insured, an individual whose property was damaged by the insured, or a provider seeking reimbursement for services, all using the same value stream" ID="ID_898234588" CREATED="1708964340047" MODIFIED="1708964512710"/>
+<node TEXT="The customer in each case is &quot;entitled&quot; to being able to submit a claim request, having the claim vetted or validated, and being made whole in one way or another" ID="ID_1814241039" CREATED="1708964513319" MODIFIED="1708964551451"/>
+<node TEXT="These &quot;entitlements&quot; are associated with various insurance products with differences for various product types" ID="ID_1008298275" CREATED="1708964553295" MODIFIED="1708964579417"/>
+<node TEXT="Investing in the universe of entitlements for a given product or products ensures that the value delivered to teh customer through a given value stream is maximized and delivery streamlined" ID="ID_1130103147" CREATED="1708964579616" MODIFIED="1708964647800"/>
+</node>
+</node>
+<node TEXT="Value Streams mapping with Capabilities" POSITION="bottom_or_right" ID="ID_1793323670" CREATED="1708964777904" MODIFIED="1708964817657">
+<hook URI="settle-claim-value-stream-mapping-capability.png" SIZE="0.4882018" NAME="ExternalObject"/>
+</node>
+<node TEXT="In Business Architecture terms, Customer Service is equivalent to Product Entitlement" FOLDED="true" POSITION="bottom_or_right" ID="ID_999065932" CREATED="1708964975616" MODIFIED="1708964997864">
+<node ID="ID_1500468091" CREATED="1708964997873" MODIFIED="1708965086282"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Product Entitlement is defined as a specified aspect of a product that represents an inherent <span style="text-decoration: underline;">commitment</span>&#xa0;&#xa0;made by an organization to a customer that is realized as an immediate, on demand, or continue obligation of the organization to the customer that acquired that product
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="The ability or inability to keep or meet these commitments determines if an organization:" ID="ID_348329508" CREATED="1708965087906" MODIFIED="1708965117177">
+<node TEXT="Provides high quality services to its customers as promised, or ends up forfeiting the customer&apos;s trust" ID="ID_618580929" CREATED="1708965117180" MODIFIED="1708965237572"/>
+<node TEXT="Achieves the aforementioned (前述) benefits associated with excellent customer service that in turn lead to tangible bottom line results" ID="ID_1784632647" CREATED="1708965238128" MODIFIED="1708965305871"/>
+</node>
+<node TEXT="Bottom Line: in order to maintain existing and attract new customers, gain related financial benefits, demand more for their products and secure customers for life, organizations should prioritize investing in product entitlements." ID="ID_1752794135" CREATED="1708965316939" MODIFIED="1708965378618"/>
+</node>
+<node TEXT="Sample Product Entitlements" FOLDED="true" POSITION="bottom_or_right" ID="ID_1203599109" CREATED="1708965407636" MODIFIED="1708965415234">
+<node TEXT="Product entitlements are specified in the language of the customer, where a customer would concur (同意) that they are entitled to certain services as a consumer, owner or licensee of a product" ID="ID_170410643" CREATED="1708965415239" MODIFIED="1708965481685"/>
+<node TEXT="Product entitlements can span multiple products, which opens opportunities to consolidate delivery fo that entitlement" ID="ID_270173150" CREATED="1708965485674" MODIFIED="1708965509576"/>
+<node TEXT="Sample product entitlements are shown for 4 different products, framed in passive vs. active terms" FOLDED="true" ID="ID_392677697" CREATED="1708965511443" MODIFIED="1708965537898">
+<node TEXT="1) Vehicle Navigation Product" ID="ID_1085798952" CREATED="1708965580042" MODIFIED="1708965592387">
+<node TEXT="Destination identification" ID="ID_136392880" CREATED="1708965692402" MODIFIED="1708965700395"/>
+<node TEXT="Multiple route map plotting" ID="ID_717207833" CREATED="1708965700778" MODIFIED="1708965714394"/>
+<node TEXT="Points of interest highlighting enroute" ID="ID_318092179" CREATED="1708965708582" MODIFIED="1708965725390"/>
+<node TEXT="Cartography visualization" ID="ID_1986231544" CREATED="1708965726098" MODIFIED="1708965735048"/>
+<node TEXT="Read-out-loud communications" ID="ID_262768365" CREATED="1708965735218" MODIFIED="1708965750375"/>
+<node TEXT="Route status notification" ID="ID_1151723589" CREATED="1708965750919" MODIFIED="1708965759764"/>
+<node TEXT="Time to destination charting" ID="ID_1203082827" CREATED="1708965755625" MODIFIED="1708965765734"/>
+</node>
+<node TEXT="2) Freezer with Repair Warranty" ID="ID_1489078730" CREATED="1708965592771" MODIFIED="1708965603386">
+<node TEXT="Engage in real time appliance monitoring" ID="ID_964767300" CREATED="1708965770416" MODIFIED="1708965780845"/>
+<node TEXT="Provide automated issue diagnosis" ID="ID_349161011" CREATED="1708965781043" MODIFIED="1708965793145"/>
+<node TEXT="Communicate with status to customer" ID="ID_500225241" CREATED="1708965794340" MODIFIED="1708965802292"/>
+<node TEXT="Notify customer of issue(s)" ID="ID_1857220060" CREATED="1708965802731" MODIFIED="1708965808149"/>
+<node TEXT="Order parts" ID="ID_1133962946" CREATED="1708965808523" MODIFIED="1708965811596"/>
+<node TEXT="Schedule onsite repair" ID="ID_561989155" CREATED="1708965811755" MODIFIED="1708965818109"/>
+</node>
+<node TEXT="3) Credit Card Offering" ID="ID_837452415" CREATED="1708965606754" MODIFIED="1708965683236">
+<node TEXT="Real time balance verification" ID="ID_953557628" CREATED="1708965819392" MODIFIED="1708965827661"/>
+<node TEXT="Real time fraud detection" ID="ID_793506287" CREATED="1708965827828" MODIFIED="1708965841098"/>
+<node TEXT="Card cancellation" ID="ID_1231688595" CREATED="1708965841244" MODIFIED="1708965845451"/>
+<node TEXT="Card suspension" ID="ID_416009861" CREATED="1708965845610" MODIFIED="1708965849244"/>
+<node TEXT="Multichannel communication" ID="ID_1736349914" CREATED="1708965849410" MODIFIED="1708965856789"/>
+</node>
+<node TEXT="4) Insurance Auto Policy" ID="ID_1776448458" CREATED="1708965683393" MODIFIED="1708965689659">
+<node TEXT="Claim approval" ID="ID_1940001599" CREATED="1708965857820" MODIFIED="1708965861687"/>
+<node TEXT="Claim filing" ID="ID_1284872150" CREATED="1708965861858" MODIFIED="1708965865504"/>
+<node TEXT="Claim payout" ID="ID_1970847008" CREATED="1708965865660" MODIFIED="1708965868793"/>
+<node TEXT="Damage estimate" ID="ID_583824492" CREATED="1708965868987" MODIFIED="1708965872524"/>
+<node TEXT="Fraud investigation" ID="ID_380256698" CREATED="1708965872694" MODIFIED="1708965877852"/>
+<node TEXT="Loaner vehicle" ID="ID_1731967591" CREATED="1708965878011" MODIFIED="1708965889955"/>
+<node TEXT="Police report access" ID="ID_1080945010" CREATED="1708965890659" MODIFIED="1708965895273"/>
+<node TEXT="Vehicle repair" ID="ID_1262564040" CREATED="1708965895453" MODIFIED="1708965899254"/>
+<node TEXT="Vehicle repair shop identification" ID="ID_1785029632" CREATED="1708965899427" MODIFIED="1708965905430"/>
+<node TEXT="Vehicle protection best practices notification" ID="ID_1020626524" CREATED="1708965905595" MODIFIED="1708965916957"/>
+</node>
+</node>
+</node>
+<node TEXT="Product Entitlement Descriptions" POSITION="bottom_or_right" ID="ID_1312938815" CREATED="1708967150455" MODIFIED="1708967162831">
+<node TEXT="Product Entitlements are described in language that is readily interpreted by business professionals." ID="ID_40538589" CREATED="1708967169825" MODIFIED="1708967209729"/>
+<node TEXT="Each product entitlement has a buisness friendly, formal description" ID="ID_1422750967" CREATED="1708967210755" MODIFIED="1708967230465"/>
+<node TEXT="Organizations should try to standardize terms across related product lines, like insurance lines" ID="ID_1034751554" CREATED="1708967230857" MODIFIED="1708967252104"/>
+<node TEXT="Sample descriptions" FOLDED="true" ID="ID_1362880877" CREATED="1708967252526" MODIFIED="1708967257691">
+<node TEXT="Claim Approval: Allows for a submitted to be reviewed, validated, adjudicated, and approved for settlement." ID="ID_314676613" CREATED="1708967257693" MODIFIED="1708967289568"/>
+<node TEXT="Claim Filing: Allows claimants to submit a claim to the company through anyone of multiple channels and have it routed to the right place" ID="ID_991596380" CREATED="1708967289967" MODIFIED="1708967818311"/>
+<node TEXT="Claim Payout: Provides for the determination of the settlement amount and distribution to the appropriate parties." ID="ID_636827382" CREATED="1708967819307" MODIFIED="1708967841073"/>
+<node TEXT="Damage Estimate: Ensures that one or more determinations of cost associated with the incident are provided to the claimant and other relevant parties" ID="ID_178377191" CREATED="1708967841283" MODIFIED="1708967872014"/>
+<node TEXT="Fraud Investigation: Provides for a fraud investigation as required to ensure that a claim filing was legitimate." ID="ID_1151777116" CREATED="1708967872444" MODIFIED="1708967895077"/>
+<node TEXT="Loaner Vehicle: Facilitates access to a vehicle while the claimant and other parties await repair or replacement" ID="ID_860604930" CREATED="1708967895755" MODIFIED="1708967927942"/>
+<node TEXT="Police Report Access: Allow the claimant and related parties to gain access to the incident report from the proper authorities." ID="ID_1715546032" CREATED="1708967929739" MODIFIED="1708967957782"/>
+<node TEXT="Vehicle Repair: Verifies that all issues associated with the incident are addressed as allowed for by the customer&apos;s agreement" ID="ID_167090031" CREATED="1708967957957" MODIFIED="1708967983807"/>
+<node TEXT="Vehicle Repair Shop Identification: Ensures that the claimant is routed to the appropriate repair shop for an estimate and repairs" ID="ID_697126975" CREATED="1708967983973" MODIFIED="1708968014336"/>
+<node TEXT="Vehicle Protection Best Practices Notification: Delivers periodic information to the customer" ID="ID_1259847632" CREATED="1708968014517" MODIFIED="1708968035878"/>
+</node>
+</node>
+</node>
 <node TEXT="3. Cross Product Line Investment Scaling" ID="ID_1089560513" CREATED="1708961478453" MODIFIED="1708961487127"/>
 <node TEXT="4. Shifting to a Customer Service Focused Enterprise" ID="ID_919555450" CREATED="1708961487306" MODIFIED="1708961499100"/>
 <node TEXT="5. Moving Forward" ID="ID_1047902034" CREATED="1708961499353" MODIFIED="1708961504341"/>
