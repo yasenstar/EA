8f9359ac9633d589d4a3068195a104609ae6f6a0
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-04-30 17:33:32 -0400

    add HTML Report structure for Archi

---
+++ architool/Archi_HTML_Report.mm
@@ -0,0 +1,114 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Archi HTML Report" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1714434417494">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Understand Structure of Archi HTML Report" POSITION="bottom_or_right" ID="ID_1283016818" CREATED="1714434447060" MODIFIED="1714434567526">
+<node TEXT="Generate HTML Report from Archi" ID="ID_1847660666" CREATED="1714434571449" MODIFIED="1714434604499">
+<node TEXT="Model: ArchiSurance" ID="ID_1077118337" CREATED="1714437821129" MODIFIED="1714437858393" LINK="https://github.com/yasenstar/ArchiSurance_Practice"/>
+</node>
+<node TEXT="Query Language: alasql" ID="ID_1622770134" CREATED="1714434466445" MODIFIED="1714434506924" LINK="https://alasql-wiki.readthedocs.io/en/latest/readme.html"/>
+<node TEXT="View Tables: SHOW TABLES" ID="ID_645462536" CREATED="1714434530853" MODIFIED="1714434639338"/>
+<node TEXT="Build Archi HTML Report Data Model (RISE Editor)" ID="ID_584589265" CREATED="1714434658001" MODIFIED="1714436526464" LINK="http://www.risetobloome.com/clickonce/riseeditor/riseeditor.htm"/>
+</node>
+<node TEXT="Query in Archi HTML Report with alasql" POSITION="bottom_or_right" ID="ID_787883700" CREATED="1714434817054" MODIFIED="1714436421955">
+<node TEXT="The Query Basic" ID="ID_1672172745" CREATED="1714437113041" MODIFIED="1714437121262">
+<node TEXT="SHOW TABLES" ID="ID_140133671" CREATED="1714437121266" MODIFIED="1714437127520"/>
+<node TEXT="SELECT * FROM [table_name]" ID="ID_1124433806" CREATED="1714437127981" MODIFIED="1714437138850"/>
+</node>
+<node TEXT="Getting Specific" ID="ID_282173391" CREATED="1714437143878" MODIFIED="1714437149443">
+<node TEXT="List items with certain condition" FOLDED="true" ID="ID_1392743899" CREATED="1714437149445" MODIFIED="1714437164013">
+<node TEXT="SELECT * FROM Properties WHERE propkey = &apos;something&apos;" ID="ID_710360327" CREATED="1714437164017" MODIFIED="1714437193710"/>
+<node TEXT="SELECT name FROM Elements WHERE id = &apos;id-xxx&apos;" ID="ID_1766894298" CREATED="1714437199926" MODIFIED="1714437225471"/>
+</node>
+<node TEXT="Combining Tables" FOLDED="true" ID="ID_1422515207" CREATED="1714437228507" MODIFIED="1714437241007">
+<node TEXT="SELECT * FROM Elements JOIN Properties ON&#xa;Elements.id = Properties.conceptid&#xa;WHERE conditions (AND/OR another condition)" ID="ID_194837407" CREATED="1714437249931" MODIFIED="1714437320144"/>
+</node>
+</node>
+</node>
+<node TEXT="Analyze Archi HTML Report in Power BI" POSITION="bottom_or_right" ID="ID_75330978" CREATED="1714437454097" MODIFIED="1714437473190">
+<node TEXT="Export HTML Report Table into CSV" ID="ID_1725747005" CREATED="1714437473198" MODIFIED="1714437492430">
+<node TEXT="select * into csv(&quot;Elements.csv&quot;) from Elements" ID="ID_1810257341" CREATED="1714438236990" MODIFIED="1714438250277"/>
+</node>
+<node TEXT="Load CSV files into Power BI Desktop" ID="ID_1665560999" CREATED="1714437492925" MODIFIED="1714437523438"/>
+<node TEXT="Build Data Model (relationships) in Power BI" ID="ID_1701920525" CREATED="1714437523885" MODIFIED="1714437539037"/>
+<node TEXT="Create Visuals in Power BI for Archi Model Analysis" ID="ID_1115889624" CREATED="1714437540391" MODIFIED="1714437579406"/>
+</node>
+</node>
+</map>
