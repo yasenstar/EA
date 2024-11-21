8b5a9e1622ef16d30433bb3dd22f46012ed73d6b
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-11-21 15:17:18 +0800

    add new installation recording

--- Essential_EA/Essential_Project.mm
+++ Essential_EA/Essential_Project.mm
@@ -1,7 +1,7 @@
 <map version="freeplane 1.12.1">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="Essential Project" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1732153396006" LINK="https://enterprise-architecture.org/" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8ff" zoom="1.3310001">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_sky_element_template.mm" show_tags="UNDER_NODES"/>
+<node TEXT="Essential Project" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1732153396006" LINK="https://enterprise-architecture.org/" VGAP_QUANTITY="3 pt"><hook NAME="MapStyle" background="#d6e8e8ff" zoom="1.1">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_sky_element_template.mm" fit_to_viewport="false"/>
     <tags category_separator="::"/>
 
 <map_styles>
@@ -110,7 +110,7 @@
 </html></richcontent>
 <node TEXT="1. Software Pre-Requisites" FOLDED="true" ID="ID_511638148" CREATED="1732151350693" MODIFIED="1732151374051">
 <node TEXT="1.1 Java Runtime Environment" ID="ID_21273202" CREATED="1732151375724" MODIFIED="1732151390594">
-<node TEXT="Require v1.8 (Required to run Protege and Apache Tomcat" FOLDED="true" ID="ID_1359623039" CREATED="1732151390600" MODIFIED="1732151567725" LINK="https://www.java.com/en/download/help/index_installing.html">
+<node TEXT="Require v1.8 (Required to run Protege and Apache Tomcat" ID="ID_1359623039" CREATED="1732151390600" MODIFIED="1732151567725" LINK="https://www.java.com/en/download/help/index_installing.html">
 <node TEXT="java --version&#xa;openjdk 21.0.5 2024-10-15 LTS&#xa;OpenJDK Runtime Environment Microsoft-10377968 (build 21.0.5+11-LTS)&#xa;OpenJDK 64-Bit Server VM Microsoft-10377968 (build 21.0.5+11-LTS, mixed mode, sharing)" ID="ID_710215631" CREATED="1732154375501" MODIFIED="1732154380917"/>
 <node TEXT="Microsoft Build of OpenJDK" ID="ID_1028286196" CREATED="1732154428942" MODIFIED="1732154447685" LINK="https://learn.microsoft.com/en-gb/java/openjdk/download"/>
 </node>
@@ -135,22 +135,31 @@
 <node TEXT="1.5 (Optional) JDBC Driver" ID="ID_1219206623" CREATED="1732151501238" MODIFIED="1732151512983"/>
 </node>
 <node TEXT="2. Install Essential Core Components" ID="ID_231710363" CREATED="1732151645553" MODIFIED="1732151719084">
-<node TEXT="2.1 Widget" ID="ID_1765146568" CREATED="1732151748889" MODIFIED="1732151768741">
+<node TEXT="2.1 Widget" FOLDED="true" ID="ID_1765146568" CREATED="1732151748889" MODIFIED="1732151768741">
 <node TEXT="v6.7 (2020-02-06)" ID="ID_1173897222" CREATED="1732151768744" MODIFIED="1732151789613" LINK="https://cdn.enterprise-architecture.org/essential-widgets/essentialinstallupgrade67.jar?_gl=1*wwb2uk*_up*MQ..*_ga*MTE5NjI4NzgyOS4xNzMyMTE0MzQz*_ga_X0G25EGQ6E*MTczMjExNDM0Mi4xLjEuMTczMjExNDQ3Ni4wLjAuMA.."/>
-<node TEXT="A set of custom-built plug-ins for the Protege Ontology Editor." ID="ID_386922087" CREATED="1732151790426" MODIFIED="1732151848989"/>
+<node TEXT="A set of custom-built plug-ins for the Protege Ontology Editor." FOLDED="true" ID="ID_386922087" CREATED="1732151790426" MODIFIED="1732151848989">
+<node TEXT="1. Update Scripts Upgrade: update installation scripts for Essential Architecture Mangaer" POSITION="bottom_or_right" ID="ID_421288966" CREATED="1732161171718" MODIFIED="1732161197282"/>
+<node TEXT="2. Widgets: Essential Architecture Manager widgets for Protege" POSITION="bottom_or_right" ID="ID_752866740" CREATED="1732161197442" MODIFIED="1732161214622"/>
+<node TEXT="3. Server: Example Essential Server configuration that provides a setup for the server element of a multi-user installation" POSITION="bottom_or_right" ID="ID_1658981692" CREATED="1732161214770" MODIFIED="1732161250035"/>
+<node TEXT="4. Essential Server Service: Service components for the Windows platform to enable the repository to run as a Windows service" POSITION="bottom_or_right" ID="ID_1823700029" CREATED="1732161250193" MODIFIED="1732161286044"/>
+<node TEXT="5. Integration Tab: Essential Integration Tab for Essential Architecture Manager" POSITION="bottom_or_right" ID="ID_624987378" CREATED="1732161286217" MODIFIED="1732161303314"/>
+<node TEXT="6. Sample Integration Data: Sample test data to run and try out and test the Essential Integration Tab, including the default &apos;importEssentialInstances&apos; transform for importing Essential XML repository snapshots" POSITION="bottom_or_right" ID="ID_1703112394" CREATED="1732161307449" MODIFIED="1732161353100"/>
+<node TEXT="7. Snapshot Tab: Essential Snapshot Tab for Essential Architecture Manager" POSITION="bottom_or_right" ID="ID_1454020306" CREATED="1732161353288" MODIFIED="1732161377873"/>
+<node TEXT="8. Update Tab: Essential Update Tab for Essential Architecture Manager" POSITION="bottom_or_right" ID="ID_1648254891" CREATED="1732161378079" MODIFIED="1732161395440"/>
+</node>
 <node TEXT=" Run the installer and follow the on-screen instructions" ID="ID_1239896188" CREATED="1732151850097" MODIFIED="1732151853913"/>
 </node>
-<node TEXT="2.2 Meta-Model" ID="ID_816459971" CREATED="1732151820661" MODIFIED="1732151826045">
+<node TEXT="2.2 Meta-Model" FOLDED="true" ID="ID_816459971" CREATED="1732151820661" MODIFIED="1732151826045">
 <node TEXT="v6.19 (2023-01-15)" ID="ID_126949896" CREATED="1732151826047" MODIFIED="1732151939077" LINK="https://cdn.enterprise-architecture.org/meta-model/essential_baseline_v6_19.zip?_gl=1*pv1e6*_up*MQ..*_ga*MTE5NjI4NzgyOS4xNzMyMTE0MzQz*_ga_X0G25EGQ6E*MTczMjExNDM0Mi4xLjEuMTczMjExNDQ3Ni4wLjAuMA.."/>
 <node TEXT="The main repository files for Essential." ID="ID_1934974300" CREATED="1732151855479" MODIFIED="1732151864311"/>
 <node TEXT="Save this to a user accessible area such as your Documents folder, unzip and then open using Protege" ID="ID_1250586665" CREATED="1732151907614" MODIFIED="1732151932899"/>
 </node>
-<node TEXT="2.3 Viewer" ID="ID_1723221380" CREATED="1732151940983" MODIFIED="1732151945151">
+<node TEXT="2.3 Viewer" FOLDED="true" ID="ID_1723221380" CREATED="1732151940983" MODIFIED="1732151945151">
 <node TEXT="v6.19.10 (2024-06-07)" ID="ID_618606655" CREATED="1732151945153" MODIFIED="1732152012418" LINK="https://cdn.enterprise-architecture.org/viewer/essential_viewer_61910.war?_gl=1*15iwpct*_up*MQ..*_ga*MTE5NjI4NzgyOS4xNzMyMTE0MzQz*_ga_X0G25EGQ6E*MTczMjExNDM0Mi4xLjEuMTczMjExNDQ3Ni4wLjAuMA.."/>
 <node TEXT="A Java Web Application for publishing, analyzing and reporting against enterprise architecture models captured using the Essential Meta-Model" ID="ID_1621614894" CREATED="1732151956659" MODIFIED="1732151992160"/>
 <node TEXT="Copy this file to Tomcat install &quot;webapp&quot; folder" ID="ID_83789234" CREATED="1732151993062" MODIFIED="1732152004795"/>
 </node>
-<node TEXT="2.4 Import Utility" ID="ID_822715791" CREATED="1732152014281" MODIFIED="1732152019792">
+<node TEXT="2.4 Import Utility" FOLDED="true" ID="ID_822715791" CREATED="1732152014281" MODIFIED="1732152019792">
 <node TEXT="v2.9.2 (2024-06-07)" ID="ID_1184906089" CREATED="1732152019794" MODIFIED="1732152068182" LINK="https://cdn.enterprise-architecture.org/import-utility/essential_import_utility_292.war?_gl=1*17opuom*_up*MQ..*_ga*MTE5NjI4NzgyOS4xNzMyMTE0MzQz*_ga_X0G25EGQ6E*MTczMjExNDM0Mi4xLjEuMTczMjExNDQ3Ni4wLjAuMA.."/>
 <node TEXT="A Java Web Application for importing existing content from spreadsheets into the Essential repository." ID="ID_1638537063" CREATED="1732152027505" MODIFIED="1732152045486"/>
 <node TEXT="Copy this file to your Tomcat install &apos;webapp&apos; folder" ID="ID_147421395" CREATED="1732152046211" MODIFIED="1732152059928"/>
@@ -162,10 +171,157 @@
 </node>
 </node>
 </node>
-<node TEXT="Apply Free EA Models" FOLDED="true" POSITION="bottom_or_right" ID="ID_1816544561" CREATED="1732152178797" MODIFIED="1732152272578" LINK="https://enterprise-architecture.org/resources/free-ea-models/">
+<node TEXT="Apply Free EA Models" POSITION="bottom_or_right" ID="ID_1816544561" CREATED="1732152178797" MODIFIED="1732152272578" LINK="https://enterprise-architecture.org/resources/free-ea-models/">
 <node TEXT="Business Capability Models" ID="ID_1369487238" CREATED="1732152201168" MODIFIED="1732152209108"/>
 <node TEXT="APQC Process Models" ID="ID_1849154864" CREATED="1732152209708" MODIFIED="1732152217904"/>
-<node TEXT="HERM (Higher Edication Reference Models)" ID="ID_908165532" CREATED="1732152218088" MODIFIED="1732152227558"/>
+<node TEXT="HERM (Higher Education Reference Models)" ID="ID_908165532" CREATED="1732152218088" MODIFIED="1732165048296">
+<node TEXT="HERM for Essential" ID="ID_560918496" CREATED="1732164529867" MODIFIED="1732164544762" LINK="https://enterprise-architecture.org/wp-content/uploads/2024/06/herm-update-files.zip">
+<node TEXT="HERM Ref in Essential" ID="ID_1426661645" CREATED="1732164635331" MODIFIED="1732164655135" LINK="https://enterprise-architecture.org/herm-higher-education-reference-models-in-essential/"/>
+<node TEXT="HERM in UCISA" ID="ID_30252869" CREATED="1732164663018" MODIFIED="1732164672473" LINK="https://www.ucisa.ac.uk/groups/enterprise-architecture-group/herm"/>
+</node>
+</node>
+<node TEXT="Troubleshooting for Update Tab" ID="ID_1381580016" CREATED="1732168916441" MODIFIED="1732168926240">
+<node TEXT="Error Message got in Windows" ID="ID_762054265" CREATED="1732168926240" MODIFIED="1732168939261"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Exception in thread &quot;AWT-EventQueue-0&quot; java.lang.LinkageError: JAXB 2.1 API is being loaded from the bootstrap classloader, but this RI (from jar:file:/C:/Program%20Files/Protege_3.5/plugins/com.enterprise_architecture.essential.update/jaxb-impl.jar!/com/sun/xml/bind/v2/model/impl/ModelBuilder.class) needs 2.2 API. Use the endorsed directory mechanism to place jaxb-api.jar in the bootstrap classloader. (See http://java.sun.com/j2se/1.6.0/docs/guide/standards/)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at com.sun.xml.bind.v2.model.impl.ModelBuilder.&lt;clinit&gt;(ModelBuilder.java:177)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at com.sun.xml.bind.v2.runtime.JAXBContextImpl.getTypeInfoSet(JAXBContextImpl.java:450)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at com.sun.xml.bind.v2.runtime.JAXBContextImpl.&lt;init&gt;(JAXBContextImpl.java:298)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at com.sun.xml.bind.v2.runtime.JAXBContextImpl.&lt;init&gt;(JAXBContextImpl.java:141)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at com.sun.xml.bind.v2.runtime.JAXBContextImpl$JAXBContextBuilder.build(JAXBContextImpl.java:1157)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at com.sun.xml.bind.v2.ContextFactory.createContext(ContextFactory.java:145)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at com.sun.xml.bind.v2.ContextFactory.createContext(ContextFactory.java:236)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at sun.reflect.NativeMethodAccessorImpl.invoke(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at sun.reflect.DelegatingMethodAccessorImpl.invoke(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.lang.reflect.Method.invoke(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at javax.xml.bind.ContextFinder.newInstance(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at javax.xml.bind.ContextFinder.find(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at javax.xml.bind.JAXBContext.newInstance(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at javax.xml.bind.JAXBContext.newInstance(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at com.enterprise_architecture.essential.update.EssentialUpdateTab.loadUpdatePackInfo(EssentialUpdateTab.java:777)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at com.enterprise_architecture.essential.update.EssentialUpdateTab.actionPerformed(EssentialUpdateTab.java:618)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at javax.swing.AbstractButton.fireActionPerformed(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at javax.swing.AbstractButton$Handler.actionPerformed(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at javax.swing.DefaultButtonModel.fireActionPerformed(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at javax.swing.DefaultButtonModel.setPressed(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at javax.swing.plaf.basic.BasicButtonListener.mouseReleased(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.Component.processMouseEvent(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at javax.swing.JComponent.processMouseEvent(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.Component.processEvent(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.Container.processEvent(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.Component.dispatchEventImpl(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.Container.dispatchEventImpl(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.Component.dispatchEvent(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.LightweightDispatcher.retargetMouseEvent(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.LightweightDispatcher.processMouseEvent(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.LightweightDispatcher.dispatchEvent(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.Container.dispatchEventImpl(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.Window.dispatchEventImpl(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.Component.dispatchEvent(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.EventQueue.dispatchEvent(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.EventDispatchThread.pumpOneEventForFilters(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.EventDispatchThread.pumpEventsForFilter(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.EventDispatchThread.pumpEventsForHierarchy(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.EventDispatchThread.pumpEvents(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.EventDispatchThread.pumpEvents(Unknown Source)
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;&#xa0;at java.awt.EventDispatchThread.run(Unknown Source)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Discussion Thread in Forum" ID="ID_1642176445" CREATED="1732168949982" MODIFIED="1732168968475" LINK="https://enterprise-architecture.org/forums/viewtopic.php?f=21&amp;t=2068&amp;p=29873&amp;hilit=java.lang.LinkageError#p29873"/>
+<node TEXT="Solved by install Eclipse JDK 8 / Oracle Java 8" ID="ID_825096210" CREATED="1732172272411" MODIFIED="1732172402620" LINK="https://www.java.com/en/download/help/index_installing.html"/>
+</node>
 </node>
 </node>
 </map>
